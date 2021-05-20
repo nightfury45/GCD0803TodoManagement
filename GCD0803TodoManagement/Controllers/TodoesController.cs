22d089693364e66ebfc56876e5c16ea6e16b14c4
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-20 10:47:36 +0700

    Create Todo class and basic Index action of TodoesController

---
+++ GCD0803TodoManagement/Controllers/TodoesController.cs
@@ -0,0 +1,32 @@
+﻿using GCD0803TodoManagement.Models;
+using System;
+using System.Web.Mvc;
+
+namespace GCD0803TodoManagement.Controllers
+{
+	public class TodoesController : Controller
+	{
+		private Todo _todo;
+
+		public TodoesController()
+		{
+			_todo = new Todo()
+			{
+				Id = 1,
+				Category = "Family",
+				Description = "Buy Milk",
+				DueDate = DateTime.Now
+			};
+		}
+		// GET: Todoes
+		public ActionResult Index()
+		{
+			return View(_todo);
+		}
+
+		public ActionResult Details()
+		{
+			return View();
+		}
+	}
+}
---
+++ GCD0803TodoManagement/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GCD0803TodoManagement.Models
+{
+	public class Todo
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public DateTime DueDate { get; set; }
+		public string Category { get; set; }
+	}
+}
