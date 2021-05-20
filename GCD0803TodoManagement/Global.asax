e7317f647abc2481dcc69227a2ac9e05d039e2a3
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-20 10:26:57 +0700

    Add project files.

---
+++ GCD0803TodoManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GCD0803TodoManagement.MvcApplication" Language="C#" %>
---
+++ GCD0803TodoManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GCD0803TodoManagement.Startup))]
+namespace GCD0803TodoManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
