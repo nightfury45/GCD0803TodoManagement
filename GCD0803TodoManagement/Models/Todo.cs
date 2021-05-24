269932fbb3c1a69ca3ef72d1d69295278cc8b561
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-24 10:00:44 +0700

    Add Required validation to Todo model

--- GCD0803TodoManagement/Models/Todo.cs
+++ GCD0803TodoManagement/Models/Todo.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCD0803TodoManagement.Models
 {
 	public class Todo
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Description { get; set; }
+		[Required]
 		public DateTime DueDate { get; set; }
+		[Required]
 		public string Category { get; set; }
 	}
 }
