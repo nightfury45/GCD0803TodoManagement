e429f10675da4d5ed4081dd7ba422a44bcd043a7
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-27 08:51:29 +0700

    Refactor TodoesController

--- GCD0803TodoManagement/Controllers/TodoesController.cs
+++ GCD0803TodoManagement/Controllers/TodoesController.cs
@@ -1,6 +1,7 @@
 ﻿using GCD0803TodoManagement.Models;
 using Microsoft.Ajax.Utilities;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace GCD0803TodoManagement.Controllers
@@ -27,7 +28,7 @@ namespace GCD0803TodoManagement.Controllers
 
 		public ActionResult Details(int? id)
 		{
-			if (id == null) return HttpNotFound();
+			if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
 			var todo = _context.Todoes.SingleOrDefault(t => t.Id == id);
 
@@ -38,7 +39,7 @@ namespace GCD0803TodoManagement.Controllers
 
 		public ActionResult Delete(int? id)
 		{
-			if (id == null) return HttpNotFound();
+			if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
 			var todo = _context.Todoes.SingleOrDefault(t => t.Id == id);
 
@@ -80,7 +81,7 @@ namespace GCD0803TodoManagement.Controllers
 		[HttpGet]
 		public ActionResult Edit(int? id)
 		{
-			if (id == null) return HttpNotFound();
+			if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
 			var todoInDb = _context.Todoes.SingleOrDefault(t => t.Id == id);
 
@@ -92,6 +93,10 @@ namespace GCD0803TodoManagement.Controllers
 		[HttpPost]
 		public ActionResult Edit(Todo todo)
 		{
+			if (!ModelState.IsValid)
+			{
+				return View(todo);
+			}
 			var todoInDb = _context.Todoes.SingleOrDefault(t => t.Id == todo.Id);
 
 			if (todoInDb == null) return HttpNotFound();
