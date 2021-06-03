d649655f938ba1a0f471f20c489dd55ac21cede9
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-03 08:37:02 +0700

    Feature: See User Information

---
+++ GCD0803TodoManagement/Controllers/UserInfosController.cs
@@ -0,0 +1,27 @@
+﻿using GCD0803TodoManagement.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GCD0803TodoManagement.Controllers
+{
+	[Authorize]
+	public class UserInfosController : Controller
+	{
+		private ApplicationDbContext _context;
+		public UserInfosController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		// GET: UserInfos
+		public ActionResult Index()
+		{
+			var userId = User.Identity.GetUserId();
+			var userInfo = _context.UsersInfos.SingleOrDefault(u => u.UserId.Equals(userId));
+
+			if (userInfo == null) return HttpNotFound();
+
+			return View(userInfo);
+		}
+	}
+}
