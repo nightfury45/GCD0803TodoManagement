db9ac277aaacdd1b366c5d1b99a9949189caa16c
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-03 08:11:47 +0700

    Update RegisterViewModel: Add FullName and Age properties

--- GCD0803TodoManagement/Models/AccountViewModels.cs
+++ GCD0803TodoManagement/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace GCD0803TodoManagement.Models
 		[Display(Name = "Confirm password")]
 		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
 		public string ConfirmPassword { get; set; }
+		public string FullName { get; set; }
+		public int Age { get; set; }
 
 	}
 
